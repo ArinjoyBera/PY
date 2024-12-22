93954f654d08adac7c5972702d199830295c1b48
ArinjoyBera <arinjoybera@gmail.com>
2024-12-22 18:23:46 +0530

    bitplay 2

---
+++ bit Play 2/equal or not.py
@@ -0,0 +1,8 @@
+def equal(num1, num2):
+    if num1^num2!=0:
+        print(num1, "is not equal to", num2)
+    else:
+        print(num1, "is equal to", num2)
+num1=int(input("Enter the number:"))
+num2=int(input("Enter another number:"))
+equal(num1, num2)
