211565ff87b7ea0db82d60466be273d362a12b68
ArinjoyBera <arinjoybera@gmail.com>
2025-01-18 18:34:56 +0530

    Surging with Power 2

---
+++ Surging with Power/Po4.py
@@ -0,0 +1,15 @@
+def nnrjr(number):
+    count = 0
+    if ((number & (~(number-1))) == number):
+        while (number > 1):
+            number>>=1
+            count += 1
+        if count%2 == 0:
+            return True
+        else:
+            return False
+NNRJR = int(input("Enter a nnrjr number:"))
+if nnrjr(NNRJR)==True:
+    print(NNRJR, "is a power of 4.")
+elif nnrjr(NNRJR)==False:
+    print(NNRJR, "is not a power of 4.")
