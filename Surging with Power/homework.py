d3eebfb6016158ddf44d8f17a21d6759e617b9ed
ArinjoyBera <arinjoybera@gmail.com>
2025-01-19 18:21:14 +0530

    Homework

---
+++ Surging with Power/homework.py
@@ -0,0 +1,15 @@
+def nnrjr(number):
+    count = 0
+    if ((number & (~(number-1))) == number):
+        while (number > 1):
+            number>>=2
+            count += 2
+        if count%2 == 0:
+            return True
+        else:
+            return False
+NNRJR = int(input("Enter a nnrjr number:"))
+if nnrjr(NNRJR)==True:
+    print(NNRJR, "is a power of 8.")
+elif nnrjr(NNRJR)==False:
+    print(NNRJR, "is not a power of 8.")
